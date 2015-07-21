171583a0e762798d9c8fad9fd63ac65798cc676b
redaphid <a@octoblu.com>
2015-07-21 16:11:55 -0700

    added examples

---
+++ examples/message.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+meshblu-util message -u ff12c403-04c7-4e63-9073-2e3b1f8e4450 -f ./message-on.json ./subscriber.json
---
+++ examples/subscribe.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+meshblu-util subscribe -u 449fdc9a-9378-4b7d-889a-a03e6615f09c ./subscriber.json
